96dd417207de641219d52bd3d9d5b87f296b5ab3
Kartik <125755557+Kartik11O@users.noreply.github.com>
2023-05-23 17:09:20 +0530

    Add files via upload

---
+++ UI Project-6(Page1)/JS/Nav.js
@@ -0,0 +1,23 @@
+var NavPhone = document.getElementById("NavPhone")
+var Page_1 = document.getElementById("Page_1")
+
+function Open_Close(){
+   
+    if(NavPhone.style.display == "none"){
+        NavPhone.style.display = "block"
+    }
+
+    else{
+        NavPhone.style.display = "none"
+        Page_1.style.display = "block"
+    }
+
+
+
+}
+
+window.addEventListener("load" , ()=>{
+
+    NavPhone.style.display = "none"
+
+})
